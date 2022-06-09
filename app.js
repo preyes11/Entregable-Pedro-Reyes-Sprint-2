53ff9e58797a8c8ebae61f8eb8a6acf7c2db05fe
Pedro Reyes <pedrovago2002@gmail.com>
2022-06-09 07:33:51 -0300

    Modificacion js y html (Ayuda de Yanina)

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const nombres = []; //Faltaban declarar las listas de nombres y de montos (sino siempre ibas a tener un solo el último nombre)
+const montos = []; //idem pero con montos
+const resultados = document.getElementById("list-group") //Faltaba declarar un espacio de lista en el html y levantarlo desde acá
+
 function envio() {
     const nombre = document.getElementById("nombre")
     const monto = document.getElementById("monto");
 
     if (nombre != "" && monto.value != "") {
-        sumaTotal(monto.value);
-        agregarPersonas(nombre, monto.value);
+        sumaTotal(monto.value); 
+        agregarPersonas(nombre.value, monto.value); //Faltaba el  ".value" despues de nombre por lo cual no podia levantar bien el valor por eso te aparecia el ObjectHtml...
 
         nombre.value ="";
         monto.value ="";
@@ -19,21 +23,25 @@ let montoTotal = 0;
 function sumaTotal(monto) {
     let Total = document.getElementById("Total");
     montoTotal = montoTotal + parseInt (monto);
-    Total.innerHTML = ' $ {montoTotal} ';
+    Total.innerHTML = montoTotal;
 }
 
 let cantPeople = 0;
 
 const agregarPersonas =(nombre, monto) => {
-    const Totales = document.getElementById("Totales");
-    let persona = document.createElement("p");
-    const texto = nombre + " : $" + monto;
-    persona.innerHTML = texto;
-    resultados.appendChild(persona);
+    //no existe el id "Totales" const Totales = document.getElementById("Totales");
+    nombres.push(nombre); //Agrego el nombre a la lista de nombres
+    montos.push(monto); //idem con montons
 
+    const li = document.createElement('li'); //inicio un elemento de lista   (<p> es párrafo)
+    li.classList.add('list-group-item'); //Agrego mi lista al html
+    const text = document.createTextNode(`${nombres[nombres.length - 1]}: $${montos[montos.length - 1]}` ); //Armo el texto de cada línea. esto da como resultado "nombre: $monto
+    li.append(text); //agrego mi texto (el anterior) a mi lista
+    resultados.appendChild(li); //agrego mi elemento a "resultados"
+    
     cantPeople++;
 
     const CUPaga = document.getElementById("CUPaga");
     const montoAPagarCU = parseINT(montoAPagarCU) / cantPeople;
-    CUPaga.innerHTML = "$" + montoAPagarCU;
+    CUPaga.innerHTML = "$" + montoAPagarCU; 
 }
